b1d10eee19c9df6c23c62ba2e43e2847f77bf54c
Kristina <kr.hranovska@gmail.com>
2024-04-21 13:59:05 +0300

    Update contacts.js

--- src/contacts.js
+++ src/contacts.js
@@ -40,14 +40,12 @@ async function removeContact(contactId) {
 
 }
 
-async function addContact(name, email, phone) {
+async function addContact(data) {
     try {
         const dataContacts = await listContacts();
         const newContact = {
             id: uuidv4(),
-            name,
-            email,
-            phone
+            ...data
         }
         dataContacts.push(newContact)
         await fs.writeFile(contactsPath, JSON.stringify(dataContacts, null, 2))
